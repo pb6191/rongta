f77c05b329e4490560686f6e2bfe2fad3c42c57d
user274308 <user274308@telehouse-moboware.dmarc.si1.atlanticmetro.net>
2025-02-17 10:30:54 -0500

    ios b8 ok

--- native-mobile-app-shell-master/ios/Flutter/flutter_export_environment.sh
+++ native-mobile-app-shell-master/ios/Flutter/flutter_export_environment.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/user271263/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/user271263/Documents/rongta/native-mobile-app-shell-master"
+export "FLUTTER_ROOT=/Users/user274308/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/user274308/Documents/rongta/native-mobile-app-shell-master"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
-export "FLUTTER_BUILD_NUMBER=7"
+export "FLUTTER_BUILD_NUMBER=8"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
